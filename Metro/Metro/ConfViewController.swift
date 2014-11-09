317199d2a0a568a49deaa32741d570bbf6443169
iam4570 <iam4570@yahoo.co.jp>
2014-11-10 06:47:50 +0900

    svprogress add

--- Metro/Metro/ConfViewController.swift
+++ Metro/Metro/ConfViewController.swift
@@ -13,6 +13,8 @@ import UIKit
 class ConfViewController: UIViewController,UITextFieldDelegate{
     
 
+    @IBOutlet weak var closeImage: UIImageView!
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var textview: UITextField!
     @IBOutlet weak var finishButton: UIButton!
     @IBOutlet weak var closeButton: UIButton!
@@ -26,8 +28,10 @@ class ConfViewController: UIViewController,UITextFieldDelegate{
         var userDef = NSUserDefaults.standardUserDefaults()
         if userDef.boolForKey("firstconfig"){
             closeButton.alpha = 0
+            closeImage.alpha = 0
         }else{
             closeButton.alpha = 1
+            closeImage.alpha = 1
         }
     }
     
@@ -49,39 +53,58 @@ class ConfViewController: UIViewController,UITextFieldDelegate{
     }
     
     func stationNameValidate(stationName:String)->Bool{
+        
+        
         var filePath = NSBundle.mainBundle().pathForResource("stnamehash", ofType:"json")
         print(filePath)
         var data = NSData(contentsOfFile: filePath!)
 
         var json = JSON.parse(NSString(data:data!, encoding: NSUTF8StringEncoding) as String)
 
-        print("length=\(json.length)")
+        println("length=\(json.length)")
         for key in json.generate() {
             if stationName == (key.0 as String){
+                SVProgressHUD.dismiss()
                 return true
             }
             let firstobj = key.1.asDictionary
-            var romatmp = firstobj!.values.first!.asString
-            let romaName:String = split(romatmp!,{ $0 == "."})[3]
-            if stationName.lowercaseString == romaName.lowercaseString{
-                return true
+            
+            var romatmp:String? = firstobj?.values.first?.asString
+            if romatmp != nil {
+                println("in the parensis")
+                println(romatmp)
+                let romaName:String = split(romatmp!,{ $0 == "."})[3]
+                if stationName.lowercaseString == romaName.lowercaseString{
+                    SVProgressHUD.dismiss()
+                    return true
+                }
             }
         }
-        
+        SVProgressHUD.dismiss()
         return false
+    }
+    func dispatch_async_main(block: () -> ()) {
+        dispatch_async(dispatch_get_main_queue(), block)
+    }
+    
+    func dispatch_async_global(block: () -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
     }
     
     @IBAction func finishButton(sender: AnyObject) {
-        if stationNameValidate(textview.text) {
-            var userDef = NSUserDefaults.standardUserDefaults()
-            userDef.setBool(false, forKey: "firstconfig")
-            println(self.textview.text)
-            ud.setObject(self.textview.text, forKey: "station")
-            ud.synchronize()
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }else{
-            
-        }
+        SVProgressHUD.show()
+      
+            if stationNameValidate(textview.text) {
+                var userDef = NSUserDefaults.standardUserDefaults()
+                userDef.setBool(false, forKey: "firstconfig")
+                println(self.textview.text)
+                ud.setObject(self.textview.text, forKey: "station")
+                ud.synchronize()
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }else{
+                errorLabel.text = "駅名を入力してください"
+            }
+        
     }
     @IBAction func closeButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
